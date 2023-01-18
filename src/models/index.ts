ca4e7c9268771b4af69889ec6ca7a344de5278e9
Lhoussaine bouaik <bouaik.lhou@gmail.com>
2023-01-18 19:34:34 +0100

    specify dialect

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,9 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_URL);
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+});
 
 async function connectToDB(): Promise<void> {
   try {
